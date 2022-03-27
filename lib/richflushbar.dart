bcb6ac500be4af0746b1bc4ef0672444c1152d19
Dammy Richie <formatable2018@gmail.com>
2022-03-27 13:16:20 +0100

    first commit

---
+++ lib/richflushbar.dart
@@ -0,0 +1,17 @@
+library richflushbar;
+
+import 'package:flutter/material.dart';
+
+class RichFlushbar extends StatefulWidget {
+  const RichFlushbar({Key? key}) : super(key: key);
+
+  @override
+  State<RichFlushbar> createState() => _RichFlushbarState();
+}
+
+class _RichFlushbarState extends State<RichFlushbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
